ca74f754fc31f0c0a0375661a6c45033d4dde4a8
UEFI <1092700291@qq.com>
2022-06-13 08:45:11 -0400

    Bug fix

--- MidiDataset.py
+++ MidiDataset.py
@@ -3,21 +3,30 @@ import miditoolkit
 import os
 
 class midiData():
-    def __init__(self, pathdir, batchsize):
-        self.midiPath = []
-        midiList = os.listdir(pathdir)
+    def __init__(self, MelodyDir, BassDir, batchsize):
+        self.MelodyPath = []
+        self.BassPath = []
+        melodyList = os.listdir(MelodyDir)
+        BassList = os.listdir(BassDir)
         self.batchsize = batchsize
-        for i in range(len(midiList)):
-            self.midiPath.append(pathdir + '/' + midiList[i])
+        for i in melodyList:
+            if i in BassList:
+                self.MelodyPath.append(MelodyDir + '/' + i)
+                self.BassPath.append(BassDir + '/' + i)
 
-    def ReadMidi(self, idx):
-        mido_obj = miditoolkit.midi.parser.MidiFile(self.midiPath[idx])
+    def ReadMidi(self, track, idx):
+        if track == 'M':
+            mido_obj = miditoolkit.midi.parser.MidiFile(self.MelodyPath[idx])
+        else:
+            mido_obj = miditoolkit.midi.parser.MidiFile(self.BassPath[idx])
         NoteSet = []
-        lastEnd = mido_obj.instruments[0].notes[0].start #Bug bypass
+        #lastEnd = mido_obj.instruments[0].notes[0].start #Bug bypass
+        lastStart = -1
+
         for i in mido_obj.instruments[0].notes:
-            if i.start == lastEnd:
+            if i.start != lastStart:
                 #Atarashi Note
-                lastEnd = i.end
+                lastStart = i.start
                 NoteSet.append([i.pitch, i.velocity, 0, 0, 0, 0])
                 t = 2
             else:
@@ -28,27 +37,39 @@ class midiData():
         return NoteSet
     
     def DataClean(self):
-        for i in range(len(self.midiPath)):
+        for i in range(len(self.MelodyPath)):
             try:
-                testResult = self.ReadMidi(i)
-                if len(testResult) < 8:
-                    #os.remove(self.midiPath[i])
-                    self.midiPath.pop(i)
-                    print('kill ' + self.midiPath[i])
+                testResultM = self.ReadMidi('M', i)
+                testResultB = self.ReadMidi('B', i)
+                if len(testResultM) < 8 or len(testResultB) < 8:
+                    #os.remove(self.MelodyPath[i])
+                    #os.remove(self.BassPath[i])
+                    self.MelodyPath.pop(i)
+                    self.BassPath.pop(i)
+                    print(testResultM)
+                    print('kill ' + self.MelodyPath[i])
+                    print('kill ' + self.BassPath[i])
             except:
-                    #os.remove(self.midiPath[i])
-                    self.midiPath.pop(i)
-                    print('kill ' + self.midiPath[i])
+                    #os.remove(self.MelodyPath[i])
+                    #os.remove(self.BassPath[i])
+                    self.MelodyPath.pop(i)
+                    self.BassPath.pop(i)
+                    print('kill ' + self.MelodyPath[i])
+                    print('kill ' + self.BassPath[i])
 
     def MakeBatch(self, batchID):
-        myBatch = []
+        myBatchM = []
+        myBatchB = []
+        batchID = int(batchID * len(self.MelodyPath) / self.batchsize)
         for i in range(self.batchsize):
-            tempID = (batchID + i) % len(self.midiPath)
-            myBatch.append(self.ReadMidi(tempID))
-        return myBatch
+            tempID = (batchID + i) % len(self.MelodyPath)
+            myBatchM.append(self.ReadMidi('M', tempID))
+            myBatchB.append(self.ReadMidi('B', tempID))
+
+        return myBatchM, myBatchB
 
 
-Dataset = midiData('/hdd2/lmd/0', 5)
-#Dataset.DataClean() Don't Try this!
-sample = Dataset.MakeBatch(0)
-print(sample)
+Dataset = midiData('/hdd2/lmd/0-melody', '/hdd2/lmd/0-bass', 5)
+#Dataset.DataClean() #Don't Try this!
+sampleM, sampleB = Dataset.MakeBatch(0)
+print(sampleM)
