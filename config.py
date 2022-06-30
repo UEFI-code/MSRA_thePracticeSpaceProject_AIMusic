793e7cd921c4ebd0c04c304511428ca7921885fc
UEFI <1092700291@qq.com>
2022-06-30 08:16:54 -0400

    Add CNN Train

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 class Config():
-    midiXPath = '/hdd2/lmd/0-melody'
-    midiTPath = '/hdd2/lmd/0-bass'
+    midiXPath = '/hdd1/lmd/0-melody'
+    midiTPath = '/hdd1/lmd/0-bass'
     batchsize = 50
     epoch = 1000
     learningRate = 0.001
